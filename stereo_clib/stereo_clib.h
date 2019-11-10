78e21bebbd1e33fb03e1b52804e905282782232b
Jsbluecat <15618561709@163.com>
2019-11-10 21:43:36 +0800

    add stereo_clib

---
+++ stereo_clib/stereo_clib.h
@@ -0,0 +1,18 @@
+﻿// stereo_match.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+using namespace cv;
+using namespace std;
+// TODO: 在此处引用程序需要的其他标头。
